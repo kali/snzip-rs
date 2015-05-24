158be189ff59900405f0e6459433da6ab8bfe282
Mathieu Poumeyrol <kali@zoy.org>
2015-05-24 10:59:49 +0200

    better buffers support

--- src/bin/sncat.rs
+++ src/bin/sncat.rs
@@ -1,8 +1,11 @@
 extern crate snzip;
 
 use std::io;
+use std::env;
 
 fn main() {
-    let mut dec = snzip::Decompressor::new(io::stdin());
+    let args:Vec<_> = env::args().collect();
+    let fast = args.len() > 1 && args[1] == "--fast";
+    let mut dec = snzip::Decompressor::new(io::stdin()).fast(fast);
     io::copy(&mut dec, &mut io::stdout()).unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,29 @@
 extern crate byteorder;
 extern crate snappy;
+extern crate crc;
 
 use std::io;
 use std::io::prelude::*;
 
 use std::fmt;
 
-use byteorder::{ ReadBytesExt, LittleEndian };
+use byteorder::{ ByteOrder, ReadBytesExt, LittleEndian };
 
 #[derive(Debug)]
-enum ChunkReader {
+enum ChunkType {
     StreamIdentifier,
-    CompressedData(io::Cursor<Vec<u8>>)
-}
-
-impl Read for ChunkReader {
-    fn read(&mut self, buf: &mut [u8]) -> Result<usize,io::Error> {
-        match *self {
-            ChunkReader::StreamIdentifier => Ok(0),
-            ChunkReader::CompressedData(ref mut cursor) => cursor.read(buf),
-        }
-    }
+    CompressedData,
+    RawData,
 }
 
 pub struct Decompressor<R : Read> {
+    check_crc: bool,
+    check_stream_identifier:bool,
     inner:R,
+    chunk:Option<ChunkType>,
     buf:Vec<u8>,
-    chunk:Option<ChunkReader>
+    buf_dec:Vec<u8>,
+    position: usize,
 }
 
 impl<R : Read> fmt::Debug for Decompressor<R> {
@@ -40,13 +37,33 @@ impl<R : Read> fmt::Debug for Decompressor<R> {
 impl<R : Read> Decompressor<R> {
 
     pub fn new(r:R) -> Decompressor<R> {
-        Decompressor { inner:r, buf: Vec::new(), chunk:None }
+        Decompressor { inner:r, buf: Vec::new(), buf_dec:Vec::new(),
+            chunk:None, check_crc: true, check_stream_identifier: true,
+            position: 0
+        }
     }
 
-    fn load_chunk(&mut self) -> Result<Option<ChunkReader>, io::Error> {
+    pub fn check_crc(self, v:bool) -> Decompressor<R> {
+        Decompressor { check_crc:v, .. self }
+    }
+
+    pub fn check_stream_identifier(self, v:bool) -> Decompressor<R> {
+        Decompressor { check_stream_identifier:v, .. self }
+    }
+
+    pub fn fast(self, v:bool) -> Decompressor<R> {
+        Decompressor { check_stream_identifier:!v, check_crc:!v, .. self }
+    }
+
+    fn checksum(buf:&[u8]) -> u32 {
+        let crc:u32 = crc::crc32::checksum_castagnoli(buf);
+        ((crc >> 15) | (crc << 17)).wrapping_add(0xa282ead8)
+    }
+
+    fn load_chunk(&mut self) -> Result<(), io::Error> {
         let mut kind:[u8;1] = [0];
         if try!(self.inner.read(&mut kind)) == 0 {
-            return Ok(None)
+            return Ok(())
         }
         let size:usize = try!(self.inner.read_uint::<LittleEndian>(3)) as usize;
         self.buf.clear();
@@ -63,15 +80,34 @@ impl<R : Read> Decompressor<R> {
             read += more;
         }
         if kind[0] == 0xff {
-            Ok(Some(ChunkReader::StreamIdentifier))
+            if !self.check_stream_identifier || self.buf == "sNaPpY".as_bytes() {
+                self.chunk = Some(ChunkType::StreamIdentifier);
+            } else {
+                return Err(io::Error::new(io::ErrorKind::Other, "invalid sNaPpY header"))
+            }
         } else if kind[0] == 0x00 {
-            let page:Vec<u8> = try!(snappy::uncompress(&self.buf[4 ..]).ok_or(
+            let check = LittleEndian::read_u32(&self.buf);
+            self.buf_dec = try!(snappy::uncompress(&self.buf[4 ..]).ok_or(
                 io::Error::new(io::ErrorKind::Other, "incomplete page")
             ));
-            Ok(Some(ChunkReader::CompressedData(io::Cursor::new(page))))
+            if self.check_crc && check != Self::checksum(&self.buf_dec) {
+                return Err(io::Error::new(io::ErrorKind::Other, "invalid crc for snappy page"))
+            } else {
+                self.position = 0;
+                self.chunk = Some(ChunkType::CompressedData);
+            }
+        } else if kind[0] == 0x01 {
+            let check = LittleEndian::read_u32(&*self.buf);
+            if self.check_crc && check != Self::checksum(&self.buf[4..]) {
+                return Err(io::Error::new(io::ErrorKind::Other, "invalid crc for snappy page"))
+            } else {
+                self.position = 4;
+                self.chunk = Some(ChunkType::RawData);
+            }
         } else {
-            Ok(Some(ChunkReader::StreamIdentifier))
+            return Err(io::Error::new(io::ErrorKind::Other, "unknown page type"));
         }
+        Ok( () )
     }
 }
 
@@ -80,13 +116,16 @@ impl<R : Read> Read for Decompressor<R> {
     fn read(&mut self, buf: &mut [u8]) -> Result<usize,io::Error> {
         loop {
             let r = match self.chunk {
-                Some(ref mut c) => try!(c.read(buf)),
-                None => 0
+                Some(ChunkType::CompressedData) => try!((&self.buf_dec[self.position ..]).read(buf)),
+                Some(ChunkType::RawData) => try!((&self.buf[self.position ..]).read(buf)),
+                _ => 0
             };
             if r>0 {
+                self.position += r;
                 return Ok(r);
             }
-            self.chunk = try!(self.load_chunk());
+            self.chunk = None;
+            try!(self.load_chunk());
             if self.chunk.is_none() {
                 return Ok(0);
             }
