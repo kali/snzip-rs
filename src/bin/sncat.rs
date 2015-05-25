523bdf2416fdacbe5b3e7a33a9b1435d0723b349
Mathieu Poumeyrol <kali@zoy.org>
2015-05-25 09:34:45 +0200

    -d

--- src/bin/sncat.rs
+++ src/bin/sncat.rs
@@ -1,13 +1,36 @@
 extern crate snzip;
+extern crate rustc_serialize;
+extern crate docopt;
+
+use docopt::Docopt;
 
 use std::io;
-use std::env;
 
-use snzip::framing::Decompressor;
+use snzip::framing::{ Compressor, Decompressor };
+
+static USAGE: &'static str = "
+Usage: sncat [--fast] [-d]
+
+Options:
+    --fast              Dont'check CRC.
+    -d, --decompress    Decompress
+";
+
+#[derive(RustcDecodable, Debug)]
+struct Args {
+    flag_fast: bool,
+    flag_decompress: bool
+}
 
 fn main() {
-    let args:Vec<_> = env::args().collect();
-    let fast = args.len() > 1 && args[1] == "--fast";
-    let mut dec = Decompressor::new(io::stdin()).fast(fast);
-    io::copy(&mut dec, &mut io::stdout()).unwrap();
+    let args: Args = Docopt::new(USAGE)
+                    .and_then(|d| d.decode())
+                    .unwrap_or_else(|e| e.exit());
+    if args.flag_decompress {
+        let mut dec = Decompressor::new(io::stdin()).fast(args.flag_fast);
+        io::copy(&mut dec, &mut io::stdout()).unwrap();
+    } else {
+        let mut comp = Compressor::new(io::stdout());
+        io::copy(&mut io::stdin(), &mut comp).unwrap();
+    }
 }
